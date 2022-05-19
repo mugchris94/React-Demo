fc47322acffeef6be1738c4957a0c597e5ed978d
mugchris94 <mugishachristian0@gmail.com>
2022-05-19 18:51:12 +0200

    Made changes on package.json and created folders for components

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+
+import '../styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+       
+      
+    </div>
+  );
+}
+
+export default App;
