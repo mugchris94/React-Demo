3c0ee011f399254633afd988dd9ec348d53fab4f
unknown <ITLAP-06@ITLAP-02.gtbank.local>
2022-06-16 22:31:41 +0200

    Dashboard Design with icons

--- src/App_Views/DashboardView.js
+++ src/App_Views/DashboardView.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import data from '../Data_json/mock-data.json';
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import icon from '../images/instagram.png';
+import calendar from '../images/calendar.png';
 import '../styles/view.css';
-
+import '../styles/dashStats.css';
 
 const DashboardView = () => {
     const [ userdetails, setUserDetail ] = useState(data);
@@ -11,9 +13,9 @@ const DashboardView = () => {
 
             <div className='menu_view'>
                 <nav>
-                    <label></label>
+                    
                     <ul>
-                        <li><span><img scr='S'/></span>Requests</li>
+                        <li><span></span>Requests</li>
                         <li><span></span>Stock_Users</li>
                         <li><span></span>Stock_items</li>
 
@@ -25,18 +27,21 @@ const DashboardView = () => {
                     <label id="addTitle">Add an Item in Stock</label>
                     <form method='#'>
                     
-                        <label> Enter name</label>
+                        <label> Enter Fullname</label>
                         <br/>
-                        <input type='text' placeholder='Enter name' value=''/><br/>
-                        <label> Enter username</label><br/>
-                        <input type='text' placeholder='Enter username' value=''/>
+                        <input type='text' name="fullname" placeholder='Enter name' value='' required />
+                        <br/>
+                        <label> Enter Address</label>
+                        <br/>
+                        <input type='text' name="address" placeholder='Enter Address' value='' required />
                         <br/>
                         <label> Enter Phone number</label><br/>
-                        <input type='text' placeholder='Enter Phone number' value=''/><br/>
-                        <label> Enter Company Name</label><br/>
-                        <input type='text' placeholder='Enter Company name' value=''/>
+                        <input type='text' name="phoneNumber" placeholder='Enter Phone number' value='' required /> 
                         <br/>
-                        <input type="submit" value='Add Item' name='addItem'/>
+                        <label> Enter Your email</label><br/>
+                        <input type='text' name="email" placeholder='Enter Company name' value='' required />
+                        <br/>
+                        <input type="submit" value='Add Item' name='addItem' required />
                         
                     </form>
                 </div>
@@ -44,9 +49,53 @@ const DashboardView = () => {
             </div>
 
             <div className='table-container'>
+
+                <div className='topheader'>
+                    <label className='dash'>Dashboard</label>
+                    <div className='dashTotals'>
+                        <div className='stats'>
+                            <img src={icon} alt='icon' />
+                              <ul>
+                                 <li>
+                                    <label>50 followers</label>
+                                 </li>
+                                 <li>
+                                    <label>10k Likes</label>
+                                 </li>
+                                
+                              </ul> 
+                        </div>
+                        <div className='stats'>
+                            <img src={icon} alt='icon' />
+                              <ul>
+                                 <li>
+                                    <label>50 followers</label>
+                                 </li>
+                                 <li>
+                                    <label>10k Likes</label>
+                                 </li>
+                                
+                              </ul> 
+                        </div>
+                        <div className='stats'>
+                            <img src={calendar} alt='calendar' />
+                              <ul>
+                                 <li>
+                                    <label>50 followers</label>
+                                 </li>
+                                 <li>
+                                    <label>10k Likes</label>
+                                 </li>
+                                
+                              </ul> 
+                        </div>  
+                    </div>
+
+                </div>
+
                 <table >
                     <thead>
-                        <tr id="table_head">
+                        <tr>
                             <th>name</th>
                             <th>username</th>
                             <th>phone</th>
@@ -63,8 +112,8 @@ const DashboardView = () => {
                                 <td>{ user_detail.phone }</td>
                                 <td>{ user_detail.company.name }</td>
                                 <td>
-                                    <button>Edit</button>||
-                                    <button>Delete</button>
+            
+                                    <button id='viewdetBtn'>View Details</button>
                                 </td>
                             </tr>
                         ))
